db830af06266aa065bb1ae4d2f6e59f4af1c3746
Sheliza Wanjiru <61009894+wanjirushee@users.noreply.github.com>
2020-08-14 21:38:36 +0300

    Update MainActivity.kt
    slight changes

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_courses.*
-import layout.CoursesRecyclerViewAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        }
+    tvRegister.setOnClickListener {
+      val intent = Intent(baseContext, RegistrationActivity::class.java)
+      startActivity(intent)
     }
+
+    btnLogin.setOnClickListener {
+      var userName = etUserName.text.toString()
+      var password = etPassword.text.toString()
+      Toast.makeText(baseContext, password, Toast.LENGTH_SHORT).show()
+      startActivity(Intent(baseContext, CoursesActivity::class.java))
+    }
+  }
+}
